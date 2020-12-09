9d87285405e00bacefef4204c8643d66c9bb863a
Hui Zhou <hzhou@hzsolutions.net>
2020-12-09 14:38:37 -0600

    update

--- out/diff_117.pl
+++ out/diff_117.pl
@@ -1,50 +1,36 @@
 #!/usr/bin/perl
 use strict;
 
-my ($n_cluster, $n_output) = @ARGV;
-if (!$n_cluster or !$n_output) {
-    die "Usage: $0 [n_cluster] [n_output]\n";
-}
-
-my @files = glob("extract-*");
-my ($n,$count, @data);
-foreach my $f (@files) {
-    print "Loading $f ...\n";
-    my ($idx, $added);
-    open In, "$f" or die "Can't open $f: $!\n";
-    while(<In>){
-        if (/^Energy avg-0/) {
-            $idx = 0;
-        }
-        elsif (defined $idx) {
-            my @tlist = split /\s+/, $_;
-            if (!$data[$idx]) {
-                $n = @tlist;
-                $data[$idx] = \@tlist;
-            }
-            elsif (abs($data[$idx]->[0] - $tlist[0]) > 0.001) {
-                print "... Energy mismatch at line $idx\n";
-                last;
-            }
-            else {
-                for (my $i = 1; $i<$n; $i++) {
-                    $data[$idx]->[$i] += $tlist[$i];
-                }
-                $added++;
-            }
-        }
-    }
-    close In;
-    if ($added) {
-        $count++;
-    }
+my ($file, $n_cluster, $n_output) = @ARGV;
+if (!-f $file or !$n_cluster or !$n_output) {
+    die "Usage: $0 $file n_cluster n_output\n";
 }
 
 my @data_out;
-foreach my $l (@data) {
-    for (my $i = 1; $i<$n; $i++) {
-        $l->[$i] /= $count;
+print "Loading $file ...\n";
+my $flag;
+open In, "$file" or die "Can't open $file: $!\n";
+while(<In>){
+    if (/^Energy avg-0/) {
+        $flag = 1;
     }
+    elsif ($flag) {
+        my @tlist = split /\s+/, $_;
+        push @data_out, get_diff(\@tlist, $n_cluster, $n_output);
+    }
+}
+close In;
+
+open Out, ">diff-out.txt" or die "Can't write diff-out.txt: $!\n";
+print "  --> [diff-out.txt]\n";
+foreach my $l (@data_out) {
+    print Out join(' ', @$l). "\n";
+}
+close Out;
+
+# ---- subroutines --------------------------------------------
+sub get_diff {
+    my ($l, $n_cluster, $n_output) = @_;
     my @ground;
     for (my $i = 1; $i<27; $i++) {
         my $j = ($i) % 9;
@@ -56,6 +42,7 @@ foreach my $l (@data) {
         }
     }
 
+    my $n = @$l;
     for (my $i = 27; $i<$n; $i++) {
         my $j = ($i) % 9;
         $l->[$i] -= $ground[$j];
@@ -71,18 +58,6 @@ foreach my $l (@data) {
         }
         push @l_out, $sum/$n_cluster;
     }
-    push @data_out, [$l->[0], @l_out];
+    return [$l->[0], @l_out];
 }
 
-open Out, ">diff.txt" or die "Can't write diff.txt: $!\n";
-print "  --> [diff.txt]\n";
-print Out "Energy";
-for (my $i = 0; $i<$n_output; $i++) {
-    print Out " out-$i";
-}
-print Out "\n";
-
-foreach my $l (@data_out) {
-    print Out join(' ', @$l). "\n";
-}
-close Out;
